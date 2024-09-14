648a07cb6a406a0654ed48c2799c28c7d3cdd82b
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2024-09-14 23:14:12 +0900

    [Silver II] Title: 쇠막대기, Time: 68 ms, Memory: 32336 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -0,0 +1,18 @@
+bar= list(input())
+answer = 0
+st = []
+
+for i in range(len(bar)):
+    if bar[i] == '(':
+        st.append('(')
+
+    else:
+        if bar[i-1] == '(': 
+            st.pop()
+            answer += len(st)
+
+        else:
+            st.pop() 
+            answer += 1 
+
+print(answer)
