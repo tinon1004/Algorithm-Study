03a8568d6f913a693387a60acc3ef706de969822
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2024-10-09 23:16:33 +0900

    [Gold V] Title: 로봇 청소기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,37 +1,48 @@
 n, m = map(int,input().split())
-r, c, d = map(int, input().split())
-maps = [list(map(int,input().split())) for _ in range(n) ]
+r, c, d = map(int,input().split())
+maps = [list(map(int,input().split())) for _ in range(n)]
 
-#북, 동, 남, 서
-dr = [-1, 0, 1, 0]
-dc = [0, 1, 0, -1]
+#북, 동,남,서
+dr=[-1, 0, 1, 0]
+dc=[0, 1, 0, -1]
 
-#방문 체크
-visited = [[0]*m for _ in range(n)]
+#범위 안 및 벽
+def maps_size(x,y):
+    return 0 <= x < n and 0 <= y < m
 
-# 시작 좌표 & 카운트
-visited[r][c] = 1
-cnt = 1
+#청소
+def cleaner(x,y,d):
+    count = 0
+    while True:
+        #1번: 청소하고 횟수 증가
+        if maps[x][y] == 0:
+            maps[x][y] = -1
+            count +=1
 
-while True:
-    flag = 0
-    for _ in range(4):
-        d = (d+3) % 4
-        nr = r + dr[d]
-        nc = c + dc[d]
+        #3번: 주변 청소 여부 탐색 및 반시계 회전
+        for _ in range(4):
+            d=(d+3) % 4
+            nx, ny = x + dr[d], y +dc[d]
+            if maps_size(nx, ny) and maps[nx][ny] == 0:
+                x,y= nx,ny
+                break # 다시 1번으로
 
-        if 0 <= nr < n and 0 <= nc < m and maps[nr][nc] == 0:
-            if visited[nr][nc] == 0:
-                visited[nr][nc] = 1
-                cnt += 1
-                r = nr
-                c = nc
-                flag = 1
-                break
-
-    if flag == 0:
-        if maps[r-dr[d]][c-dc[d]] == 1:
-            print(cnt)
-            break
+        #2번: 주변 4칸 모두 청소 완료 상태면 후진 및 벽 탐지 멈춤
         else:
-            r, c = r-dr[d], c-dc[d]
+            x,y = x + dr[d]*(-1), y +dc[d]*(-1)
+            if maps_size(x, y) and maps[x][y] == 1 :
+                print(count)
+                return
+
+cleaner(r,c,d)
+
+
+
+
+
+
+
+
+
+
+
