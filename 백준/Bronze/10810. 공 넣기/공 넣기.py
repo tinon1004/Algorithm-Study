a799acff5ec649fc5d0316de93c8097ab5ac5330
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2024-10-12 20:45:50 +0900

    [Bronze III] Title: 공 넣기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -1,11 +1,10 @@
-n, m = map(int,input().split())
-box = [0]*n
+N,M = map(int,input().split())
+info = [list(map(int,input().split())) for _ in range(M)]
+box = [0] * N
 
-for _ in range(m):
-    i,j,k = map(int,input().split())
+for num in info:
+    i,j,k = num
+    for p in range(i-1,j):
+        box[p]=k
 
-    for idx in range(i,j+1):
-        box[idx-1]=k
-
-for number in range(n):
-    print(box[number], end=" ")
+print(*box)
