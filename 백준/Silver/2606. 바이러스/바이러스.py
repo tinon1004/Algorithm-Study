c6be0ddf8460cf2ef3674dc4fc11ac090a4847ae
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2024-10-12 22:32:02 +0900

    [Silver III] Title: 바이러스, Time: 52 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -3,29 +3,31 @@ from collections import deque
 n = int(input())
 v = int(input())
 
-graph = [[] for i in range(n+1)]
-visited = [0]*(n+1) #방문한 컴퓨터인지 표시할 배열
+info = [list(map(int, input().split())) for _ in range(v)]
+graph = [[] for _ in range(n + 1)]
+visited = [0] * (n + 1)
 
-#그래프 생성
-for i in range(v):
-    a ,b = map(int,input().split())
+# 연결
+for a, b in info:
     graph[a].append(b)
     graph[b].append(a)
 
-visited[1]=1 # 1번 컴퓨터부터 시작
 
-def bfs(start):
-    q = deque()
-    q.append(start)
+def bfs(graph, start):
+    q = deque([start])
+
+    visited[start] = 1
     count = 0
 
     while q:
-        curr = q.popleft()
-        for i in graph[curr]: #curr와 연결된 다른 컴퓨터 리스트
-            if visited[i] == 0: # visited[i] 감염 여부 확인
-                q.append(i) # 현재 감염된 컴퓨터로 큐에 추가하여 다음에 탐색할 컴퓨터로 추가
-                visited[i] = 1 # 감염 기록
-                count += 1  # 감염된 컴퓨터 수를 증가시킴
+        current = q.popleft()
+        for i in graph[current]:
+            if visited[i] == 0:
+                q.append(i)
+                visited[i] = 1
+                count += 1
     return count
 
-print(bfs(1))
+
+print(bfs(graph, 1))
+
