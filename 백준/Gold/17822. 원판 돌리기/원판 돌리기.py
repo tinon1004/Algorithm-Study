5831bc0411bfe64329a7516d529f79fbcda7168c
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2024-10-13 01:06:07 +0900

    [Gold II] Title: 원판 돌리기, Time: 132 ms, Memory: 34136 KB -BaekjoonHub

--- 백준/Gold/17822. 원판 돌리기/원판 돌리기.py
+++ 백준/Gold/17822. 원판 돌리기/원판 돌리기.py
@@ -1,57 +1,73 @@
 from collections import deque
 
-# 원판의 개수, 원판에 적힌 정수의 개수, T 번 회전
-n, m, t = map(int, input().split())
-board = [deque(int(x) for x in input().split()) for _ in range(n)]
+N, M, T = map(int,input().split())
+board = [deque(map(int,input().split())) for _ in range(N)]
+rotation_list = [list(map(int,input().split())) for _ in range(T)]
 
-info = [list(map(int, input().split())) for _ in range(t)]
 
-# t번 회전
-for tc in range(t):
-    x, d, k = info[tc]
+answer=0
 
-    # 원판 회전
-    for i in range(n):
-        if (i + 1) % x == 0:
-            if d == 0:  # 시계 방향
-                board[i].rotate(k)
-            else:  # 반시계 방향
-                board[i].rotate(-k)
 
-    remove = set()  # 중복 제거를 위한 set 사용
-    # 같은 원판 내 동일 숫자 찾기
-    for i in range(n):
-        for j in range(m):
-            # 오른쪽 숫자 확인
-            if board[i][j] != 0 and board[i][j] == board[i][(j + 1) % m]:
-                remove.add((i, j))
-                remove.add((i, (j + 1) % m))
+for turn in range(T):
 
-    # 인접한 위치의 동일 숫자 찾기
-    for j in range(m):
-        for i in range(n - 1):
-            if board[i][j] != 0 and board[i][j] == board[i + 1][j]:
-                remove.add((i, j))
-                remove.add((i + 1, j))
 
-    # 제거할 위치에 숫자를 0으로 변경
-    for x, y in remove:
-        board[x][y] = 0
+        x, d, k = rotation_list[turn]
 
-    # 제거할 것이 없을 경우 평균 구하기
-    if not remove:
-        total_sum = sum(sum(board[i]) for i in range(n))
-        count = sum(1 for i in range(n) for j in range(m) if board[i][j] != 0)
-        if count > 0:
-            average = total_sum / count
 
-            for i in range(n):
-                for j in range(m):
-                    if board[i][j] < average and board[i][j] != 0:
-                            board[i][j] += 1
-                    elif board[i][j] > average and board[i][j] != 0:
+
+        #x 배수의 원판 구하기
+        # d 방향에 따라 회전시키기
+        for i in range(N):
+            # i가 0부터 시작되기 때문에 x배수는 i+1을 통해 구함
+            if (i +1)% x == 0:
+                if d == 0 :
+                    board[i].rotate(k)
+                elif d == 1:
+                    board[i].rotate(-k)
+
+        remove = set()
+
+        #1. 같은 원판 내 동일 숫자 확인
+        for i in range(N):
+            for j in range(M):
+                if board[i][j] == board[i][(j+1)%M] and board[i][j] != 0:
+                    remove.add((i,j))
+                    remove.add((i, (j+1)%M))
+
+
+    # 다른 원판 인접한 동일 숫자 확인
+        for j in range(M):
+            #제일 바깥 원판은 다음 원판과 비교할 수 없기 때문에 -1
+            for i in range(N-1):
+                if board[i][j] == board[i+1][j] and board[i][j] != 0:
+                    remove.add((i,j))
+                    remove.add((i+1, j))
+
+
+        #remove 좌표 0으로 바꾸기
+        for i,j in remove:
+            board[i][j] = 0
+
+        #2. 지울 숫자가 없는 경우 평균 구하기
+        if not remove:
+            total_sum = 0
+            count = 0
+            average = 0
+
+            total_sum = sum(sum(board[i]) for i in range(N))
+            for i in range (N):
+                for j in range(M):
+                    if board[i][j] !=0:
+                        count +=1
+            if count > 0 and total_sum > 0:
+                average = total_sum / count
+
+                for i in range(N):
+                    for j in range(M):
+                        if average > board[i][j] and board[i][j] != 0:
+                            board[i][j] +=1
+                        elif average < board[i][j] and board[i][j] != 0:
                             board[i][j] -= 1
 
-# 최종 원판 숫자 합 구하기
-answer = sum(sum(board[i]) for i in range(n))
+answer = sum(sum(board[i]) for i in range(N))
 print(answer)
