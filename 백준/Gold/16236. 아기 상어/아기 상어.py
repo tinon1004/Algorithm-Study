0cb5396dbedbbe0c11787e5d690b5b4d23612a54
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2024-10-13 00:04:24 +0900

    [Gold III] Title: 아기 상어, Time: 64 ms, Memory: 34104 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,73 +1,69 @@
 from collections import deque
 
-def bfs(si,sj):
+def bfs(i,j):
+    q=deque([(i,j)])
     flist=[]
-    q = deque()
-    visited =[[0]*N  for _ in range(N)]
-
-    q.append((si,sj))
-    visited[si][sj] =1
-    fish = 0
+    visited=[[0]*N for _ in range(N)]
+    visited[i][j]=1
+    fish =0
 
     while q:
         ci,cj = q.popleft()
-        if fish == visited[ci][cj]:
-            return flist,fish -1
 
-        # 조건에 따른 방향 이동
-        # 범위 내 , 미방문인지, 물고기 < 아기 상어
+        #fish와 visited를 공유하기 때문에 -1 필요
+        if fish == visited[ci][cj]:
+            return flist, fish-1
+
+
         for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
-            ni,nj = ci+di, cj+dj
+            ni, nj = ci + di, cj + dj
+            # 조건: 범위 내, 아직 미방문, 이동 조건인 아기상어 >= 물고기
             if 0<= ni <N and 0<= nj < N and visited[ni][nj] == 0 and shark_size >= board[ni][nj]:
                 q.append((ni,nj))
-                visited[ni][nj] = visited[ci][cj]+1
-                #아기상어가 먹을 수 있는 물고기인 경우 Flist에 추가
-                if shark_size > board[ni][nj] > 0:
+                visited[ni][nj] = visited[ci][cj] +1
+
+                #먹을 수 있는 물고기인지 확인
+                if 0 < board[ni][nj] < shark_size:
                     flist.append((ni,nj))
                     fish = visited[ni][nj]
-
-
-    return flist, fish -1
-
-
-
-
+    return flist, fish-1
 
 
 N = int(input())
 board = [list(map(int,input().split())) for _ in range(N)]
 
-#아기상어 위치 찾기
+time =0
+shark_size=2
+fish = 0
+
+#상어 위치 찾기
 for i in range(N):
     for j in range(N):
         if board[i][j] == 9:
-            ci,cj = i,j
-            board[i][j] = 0 #아기 상어 자리 0으로 바꾸기
+            board[i][j] = 0
+            ci,cj = i, j
 
-shark_size = 2
-time = 0
-fish = 0
+
+
 
 while True:
+
     flist, dist = bfs(ci,cj)
 
-    #물고기 수가 없다면 종료
-    if len(flist) == 0:
+    # 반환받은 flist가 없으면 종료
+    if not flist:
         break
 
-    #물고기 위치 정렬, 가장 왼쪽 상단 기준 가져오기,
-    #상어 위치 재설정, 원래 상어 위치는 0으로 바꾸기
-
-    ni, nj = min(flist)
+    # 가장 위쪽, 상단 물고기를 가져옴
+    ni,nj = min(flist)
     board[ni][nj] = 0
-    ci,cj = ni, nj
+    ci, cj = ni, nj
 
-    # 이동 거리, 먹은 물고기 더하기 더하기
     time += dist
-    fish += 1
+    fish +=1
 
-    # 먹은 물고기 수와 상어 사이즈가 같으면, 상어 +1, 물고기 초기화
+    #상어 사이즈
     if fish == shark_size:
-        shark_size +=1
         fish = 0
+        shark_size += 1
 print(time)
