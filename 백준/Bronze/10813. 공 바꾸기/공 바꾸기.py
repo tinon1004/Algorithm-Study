1f2005966b406c3ce9cbd8bc7571177d9a678b9b
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2024-10-13 12:43:35 +0900

    [Bronze II] Title: 공 바꾸기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,12 +1,12 @@
-n, m = map(int,input().split())
-box = [i for i in range(1,n+1)]
+n,m = map(int,input().split())
+info = [list(map(int,input().split())) for _ in range(m)]
+box=[i for i in range(1,n+1)]
 
-for _ in range(m):
-    i,j= map(int,input().split())
+for turn in info:
+    i,j = turn
+
     temp = box[i-1]
     box[i-1]=box[j-1]
-    box[j-1]= temp
+    box[j-1]=temp
 
-
-for number in box:
-    print(number, end=" ")
+print(*box)
