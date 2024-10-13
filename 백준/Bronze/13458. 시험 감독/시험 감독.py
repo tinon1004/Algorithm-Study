b9a587ec27c8c378c4d066b31012b79aa6e308bf
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2024-10-13 12:28:31 +0900

    [Bronze II] Title: 시험 감독, Time: 660 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.py
+++ 백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -1,16 +1,13 @@
-n = int(input())
-a = list(map(int,input().split()))
+N = int(input())
+student = list(map(int,input().split()))
 b,c = map(int,input().split())
 
-count=0
+count = 0
 
-for i in a:
-  count +=1
-  
-  if i-b > 0:
-        count += ((i-b) // c )
-        if (i-b) % c != 0:
-          count += 1
+for i in student:
+    count +=1
+    if i-b > 0:
+        count += (i-b) // c
+        if (i-b) % c !=0:
+            count +=1
 print(count)
-
-
