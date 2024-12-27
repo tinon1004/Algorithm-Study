a48af44b775964e785738afa9d2aa9aa09b7baea
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2024-12-27 14:15:00 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -0,0 +1,8 @@
+def solution(my_string, is_suffix):
+    if len(is_suffix) > len(my_string):
+        return 0
+    
+    if my_string[len(my_string) - len(is_suffix):] == is_suffix:
+        return 1
+    else:
+        return 0
