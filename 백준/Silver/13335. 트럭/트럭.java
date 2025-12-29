a3b7e5df1190c655e93a3c6006ffdf5ffced9295
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2025-12-29 19:22:16 +0900

    [Silver I] Title: 트럭, Time: 244 ms, Memory: 20916 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.java
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -1,56 +1,55 @@
-import java.util.Scanner;
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Main {
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt(); // 트럭 수
-    int w = sc.nextInt(); // 다리 길이
-    int L = sc.nextInt(); // 다리 최대 하중
-    //트럭 대기열 생성 -> 아직 다리에 올라가지 않은 트럭들
-    int[] trucks = new int[n]; 
+    public static int truckCrossingTime(int n, int w, int L, int[] truckWeights) {
+        Deque<Integer> bridge = new ArrayDeque<>();
+        
+        // 다리 길이만큼 0으로 초기화
+        for (int i = 0; i < w; i++) {
+            bridge.addLast(0);
+        }
 
-    //트럭 무게 순서대로 저장
-    for(int i=0; i<n; i++){
-      trucks[i] = sc.nextInt();
+        int currentWeight = 0;
+        int time = 0;
+
+        for (int truck : truckWeights) {
+            while (true) {
+                time++;
+
+                // 다리에서 가장 앞에 있는 트럭(또는 0) 제거
+                int outgoingTruck = bridge.pollFirst();
+                currentWeight -= outgoingTruck;
+
+                // 현재 트럭이 다리에 올라갈 수 있는지 확인
+                if (currentWeight + truck <= L) {
+                    bridge.addLast(truck);
+                    currentWeight += truck;
+                    break;
+                } else {
+                    // 트럭 못 올라가면 빈 공간(0) 추가
+                    bridge.addLast(0);
+                }
+            }
+        }
+
+        // 마지막 트럭이 다리를 건너는 시간
+        time += w;
+        return time;
     }
 
-    //QUEUE로 다리 만들기
-    Queue<Integer>bridge = new LinkedList<>();
-    //일단 다리 위를 0으로 초기화
-    for(int i=0; i<w; i++){
-      bridge.offer(0);
-    }
-    
-    int sum = 0; //현재 다리 위 트럭 무게 합
-    int index = 0; // 다음에 올라갈 트럭 번호
-    int time = 0; //시간
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    // 다리에 못 올라간 트럭이 남아있는 동안 반복
-    // 다리에 한 번이라도 올라갔는지가 기준이 됨
-    while(n > index) {
-      time++;
-      //다리 위 트럭 이동
-      //다리의 맨 앞 칸 제거 -> 무게합에서 제거
-      sum -= bridge.poll();
-      
-      // 새 트럭을 올릴 수 있는지 확인
-      
-      // 다리 하중을 초과하지 않으면 트럭 진입 허용
-      if (sum + trucks[index] <= L){ 
-        sum += trucks[index]; // 다리 하중 증가
-        bridge.offer(trucks[index]); // 다리 맨 뒤에 트럭 추가
-        index++; // 다음 트럭으로 이동
-      }else{
-        // 새 트럭이 올라가지 못할 때 빈칸 0
-        bridge.offer(0);
-      }
+        int n = sc.nextInt();
+        int w = sc.nextInt();
+        int L = sc.nextInt();
+
+        int[] truckWeights = new int[n];
+        for (int i = 0; i < n; i++) {
+            truckWeights[i] = sc.nextInt();
+        }
+
+        System.out.println(truckCrossingTime(n, w, L, truckWeights));
     }
-    // 마지막 트럭이 올라간 뒤 다리 길이만큼 더 이동해야 완전히 통과한 
-    time += w;
-    
-    System.out.println(time);
-  }
 }
