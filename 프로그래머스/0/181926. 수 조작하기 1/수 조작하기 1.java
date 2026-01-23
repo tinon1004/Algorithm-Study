afd6bd81a76e3beb73c438d199dabef49caf044d
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-01-23 19:41:49 +0900

    [level 0] Title: 수 조작하기 1, Time: 4.73 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = n;
+        
+        for (int i=0; i<control.length(); i++) {
+            
+            char c = control.charAt(i);
+            
+            if (c == 'w') {
+                answer += 1;
+            } else if (c == 's') {
+                answer -= 1;
+            } else if (c == 'd') {
+                answer += 10;
+            } else if (c == 'a') {
+                answer -= 10;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
