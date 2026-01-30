a5ced171f766f340512cd064b97d644f2662c8d5
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-01-31 01:34:51 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 2.90 ms, Memory: 84.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        answer.add(n);
+        
+        while (n != 1){
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = 3 * n + 1;
+            }
+            answer.add(n);
+        }
+        
+        return answer.stream().mapToInt(i->i).toArray();
+    }
+}
