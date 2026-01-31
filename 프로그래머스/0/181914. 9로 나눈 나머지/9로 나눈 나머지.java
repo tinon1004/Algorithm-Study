5d5137cd0afb4557fed447f68dc6b669e1f19b9d
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-01-31 18:40:23 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 4.42 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -1,13 +1,14 @@
 class Solution {
     public int solution(String number) {
-        int sum = 0;
-        String[] arr = number.split("");
-      
-        for ( String i : arr){
-           sum += Integer.parseInt(i);
+        int answer = 0;
+        
+        for (int i = 0; i < number.length(); i++) {
+            // 아스키코드 "0"을 빼서 문자를 정수로 바꾸는 방법
+            answer += number.charAt(i) - '0';
+            // answer += Integer.parseInt(String.valueOf(number.charAt(i)));
         }
+        answer %= 9;
         
-        return sum % 9;
-        
+        return answer;
     }
 }
