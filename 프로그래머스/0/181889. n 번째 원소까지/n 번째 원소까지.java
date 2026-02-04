1e69ef75893b0ce0c2c39ad6baddcda162bb5a77
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-02-04 17:53:04 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.04 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer;
+        
+        answer = Arrays.copyOfRange(num_list, 0, n);
+        
+        return answer;
+    }
+}
