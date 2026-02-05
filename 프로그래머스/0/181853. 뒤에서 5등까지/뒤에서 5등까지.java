da76920075fec4c4cdb47b565988a93bf9d8ede0
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-02-05 17:58:22 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.47 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[5];
+        
+        Arrays.sort(num_list);
+        
+        answer = Arrays.copyOfRange(num_list, 0, 5);
+        
+        return answer;
+    }
+}
