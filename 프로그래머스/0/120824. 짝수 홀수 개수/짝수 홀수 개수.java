df194d91e94256cdd8bbc47fe63ee23d7f8102d4
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-02-13 15:37:54 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 94.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        
+        for (int i : num_list) {
+            if (i % 2 != 0) {
+                answer[1] += 1;
+            } else {
+                answer[0] += 1;
+            }
+        }
+        
+        return answer;
+    }
+}
