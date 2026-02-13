5ca429bee176f280e3377ce36dc43df2791c5c1b
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-02-13 12:13:16 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n + 1) / 2];
+        int idx = 0;
+        
+        for (int i = 1; i <= n; i += 2) {
+            answer[idx] = i;
+            idx++;
+        }
+        
+        return answer;
+    }
+}
