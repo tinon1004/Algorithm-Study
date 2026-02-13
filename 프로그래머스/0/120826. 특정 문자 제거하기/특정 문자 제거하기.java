7f986ff16fb3055bb139b9a0f38ba9ab365fa2b5
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-02-13 15:54:42 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.04 ms, Memory: 94.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string, String letter) {
+        
+//         String[] answer = {};   
+//         answer = my_string.split(letter);     
+//         return String.join("",answer);
+        
+        String answer = "";
+        
+        answer = my_string.replace(letter, "");
+        
+        return answer;
+        
+    }
+}
