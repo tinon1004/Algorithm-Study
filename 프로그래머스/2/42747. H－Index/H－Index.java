f4a2d9fb36b0e3ef6e819ab45c007eded18f4c51
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-02-17 16:17:14 +0900

    [level 2] Title: H-Index, Time: 6.53 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int[] citations) {
+        int max = 0;
+        
+        for (int i = 1; i <= citations.length; i++) {                       int count = 0;
+                                                     
+            for (int j = 0; j < citations.length; j++) {
+                if (citations[j] >= i) {
+                    count++;
+                }
+            }
+            
+            if (count >= i) {
+                max = i;
+            }
+        }
+        
+        return max;
+    }
+}
