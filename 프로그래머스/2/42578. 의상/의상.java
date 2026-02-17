a02f702fdf6879d5887697162a79529876572d1b
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-02-17 15:20:40 +0900

    [level 2] Title: 의상, Time: 0.14 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -10,12 +10,14 @@ class Solution {
             map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
         }
         
-        for (int count : map.values()) {
-            // 해당 종류 아이템 개수 + 안 입는 경우 1
-            answer *= (count + 1);
+        
+        for (int i : map.values()) {
+            answer *= (i + 1);
         }
         
-        // 최소 1개는 입어야 하기 때문에 -1 필요
-        return answer - 1;
+        // 최소 1개라도 착용해야하기 때문에 아예 안 입는 경우 1 차감
+        answer -= 1;
+        
+        return answer;
     }
 }
