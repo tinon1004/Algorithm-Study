1a2af5740e793648f60166f9555a1701962aace5
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-02-17 15:58:23 +0900

    [level 2] Title: 가장 큰 수, Time: 2961.39 ms, Memory: 407 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 391 MB, 시간: 2883.36 ms
+메모리: 407 MB, 시간: 2961.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 16일 19:46:12
+2026년 02월 17일 15:58:21
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,25 +2,26 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
+        String answer = "";
         
         String[] arr = new String[numbers.length];
         
         for (int i = 0; i < numbers.length; i++) {
-            arr[i] = String.valueOf(numbers[i]);  
+            arr[i] = String.valueOf(numbers[i]);
         }
         
-        // (b + a) 하는 이유는 내림차순 정렬을 하기 위해
+        // compareTo()은 int[]에 사용 불가
+        // String[], Integer[]에 사용 가능
+        // arr["1"] 인 경우에는 비교 자체가 필요 없기 때문에 그대로 끝남 -> compareTo()는 두 개 이상일 때만 비교
         Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
-        
-        String answer = "";
-        
-        // 모두 0인 경우
-        if (arr[0].equals("0")) {
-            return "0";
-        }
         
         for (int i = 0; i < arr.length; i++) {
             answer += arr[i];
+        }
+       
+        // ["0","0","0"] -> "000"이 되기 때문에 따로 조건을 추가해야
+        if (arr[0].equals("0")) {
+            answer = "0";
         }
         
         return answer;
