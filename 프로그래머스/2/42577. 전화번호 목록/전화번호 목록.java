b4ef09328d75c6c4a6ae24ab27114b900c505583
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-02-17 15:13:45 +0900

    [level 2] Title: 전화번호 목록, Time: 293.02 ms, Memory: 226 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -7,10 +7,11 @@ class Solution {
         HashMap<String, Integer> map = new HashMap<>();
         
         for (int i = 0; i < phone_book.length; i++) {
-            map.put(phone_book[i], i);
+            map.put(phone_book[i],i);
         }
         
         for (int i = 0; i < phone_book.length; i++) {
+            // j=0 → "", j=1 → "1", j=2 → "11"
             for (int j = 0; j < phone_book[i].length(); j++) {
                 if (map.containsKey(phone_book[i].substring(0, j))) {
                     return false;
@@ -18,6 +19,6 @@ class Solution {
             }
         }
         
-        return answer;
+        return true;
     }
 }
