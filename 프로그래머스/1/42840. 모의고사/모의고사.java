9a8644272fd4983938aca8897ca8fbbcc0c89ae9
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-02-18 15:55:33 +0900

    [level 1] Title: 모의고사, Time: 3.51 ms, Memory: 91.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -3,23 +3,22 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
         ArrayList<Integer> answer = new ArrayList<>();
-        
-        int[] a = {1, 2, 3, 4, 5};
-        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        
         int[] score = new int[3];
         
+        int[] p1 = {1, 2, 3, 4, 5};
+        int[] p2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == a[i % a.length]) {
+            if (answers[i] == p1[i % p1.length]) {
                 score[0]++;
             };
-            if (answers[i] == b[i % b.length]) {
+            if (answers[i] == p2[i % p2.length]) {
                 score[1]++;
             };
-            if (answers[i] == c[i % c.length]) {
+            if (answers[i] == p3[i % p3.length]) {
                 score[2]++;
-            }
+            };
         }
         
         int max = Math.max(score[0], Math.max(score[1], score[2]));
@@ -27,12 +26,15 @@ class Solution {
         if (score[0] == max) {
             answer.add(1);
         };
+        
         if (score[1] == max) {
             answer.add(2);
         };
+        
         if (score[2] == max) {
             answer.add(3);
         };
+        
         
         return answer.stream().mapToInt(x -> x).toArray();
     }
