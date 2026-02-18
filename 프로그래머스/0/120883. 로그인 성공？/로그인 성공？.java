096a859b1b31c516dac89dc259a7f02ca692aae7
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-02-18 18:54:11 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "";
+        
+        HashMap<String, String> map = new HashMap<>();
+        
+        for (int i = 0; i < db.length; i++) {
+            map.put(db[i][0], db[i][1]);
+        }  
+        
+        if (map.containsKey(id_pw[0]) && map.get(id_pw[0]).equals(id_pw[1])) {
+            answer = "login";
+        } else if (map.containsKey(id_pw[0]) && !map.get(id_pw[0]).equals(id_pw[1])) {
+            answer = "wrong pw";
+        } else if (!map.containsKey(id_pw[0])) {
+            answer = "fail";
+        }
+        
+        return answer;
+    }
+}
