02fcea177a8f56d3a00169fce9fc87fcd48b9f4c
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-02-19 14:23:06 +0900

    [level 1] Title: 모의고사, Time: 4.18 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -3,38 +3,42 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
         ArrayList<Integer> answer = new ArrayList<>();
-        int[] score = new int[3];
-        
         int[] p1 = {1, 2, 3, 4, 5};
         int[] p2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] score = new int[3];
         
         for (int i = 0; i < answers.length; i++) {
             if (answers[i] == p1[i % p1.length]) {
                 score[0]++;
-            };
+            }
+        }
+        
+        for (int i = 0; i < answers.length; i++) {
             if (answers[i] == p2[i % p2.length]) {
                 score[1]++;
-            };
+            }
+        }
+        
+        for (int i = 0; i < answers.length; i++) {
             if (answers[i] == p3[i % p3.length]) {
                 score[2]++;
-            };
+            }
         }
         
         int max = Math.max(score[0], Math.max(score[1], score[2]));
         
         if (score[0] == max) {
             answer.add(1);
-        };
+        }
         
         if (score[1] == max) {
             answer.add(2);
-        };
+        }
         
         if (score[2] == max) {
             answer.add(3);
-        };
-        
+        }
         
         return answer.stream().mapToInt(x -> x).toArray();
     }
