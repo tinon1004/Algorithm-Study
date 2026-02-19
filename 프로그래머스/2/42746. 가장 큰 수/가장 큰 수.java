83740bb64aacafd8dfc9060cfdf9618717ca330f
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-02-19 17:51:33 +0900

    [level 2] Title: 가장 큰 수, Time: 2899.80 ms, Memory: 409 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -3,26 +3,24 @@ import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
         String answer = "";
+        String[] str = new String[numbers.length];
         
-        String[] arr = new String[numbers.length];
         
         for (int i = 0; i < numbers.length; i++) {
-            arr[i] = String.valueOf(numbers[i]);
+            str[i] = String.valueOf(numbers[i]);
+            
         }
         
-        // compareTo()은 int[]에 사용 불가
-        // String[], Integer[]에 사용 가능
-        // arr["1"] 인 경우에는 비교 자체가 필요 없기 때문에 그대로 끝남 -> compareTo()는 두 개 이상일 때만 비교
-        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
+        Arrays.sort(str, (a , b) -> (b + a).compareTo(a + b));
         
-        for (int i = 0; i < arr.length; i++) {
-            answer += arr[i];
+        for (int i = 0; i < str.length; i++) {
+            answer += str[i];
         }
-       
-        // ["0","0","0"] -> "000"이 되기 때문에 따로 조건을 추가해야
-        if (arr[0].equals("0")) {
+        
+        if (str[0].equals("0")) {
             answer = "0";
         }
+        
         
         return answer;
     }
