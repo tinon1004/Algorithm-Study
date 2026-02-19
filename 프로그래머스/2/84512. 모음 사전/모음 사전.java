5a80715b7c98850250a5ab5523a3b97e21bc099c
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-02-19 18:06:19 +0900

    [level 2] Title: 모음 사전, Time: 4.36 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -2,31 +2,34 @@ class Solution {
     
     int answer = 0;
     int count = 0;
-    String[] str = {"A", "E", "I", "O", "U"};
+    String[] arr = {"A", "E", "I", "O", "U"};
     
     public int solution(String word) {
         
         dfs(word, "");
-
+        
         return answer;
     }
     
     public void dfs(String word, String curr) {
+        
         
         if (word.equals(curr)) {
             answer = count;
             return;
         }
         
+        
         if (curr.length() == 5) {
             return;
         }
         
-        for(int i = 0; i < 5; i++) {
-            count++;
-            dfs(word, curr + str[i]);
-        }
+       
         
+        for (int i = 0; i < arr.length; i++) {
+            count++;
+            dfs(word, curr + arr[i]);
+        }
         
     }
 }
