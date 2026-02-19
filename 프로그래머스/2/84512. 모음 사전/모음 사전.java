0956ee51830d81dcf65214193d89014cdbd4e806
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-02-19 14:38:27 +0900

    [level 2] Title: 모음 사전, Time: 4.46 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -2,31 +2,31 @@ class Solution {
     
     int answer = 0;
     int count = 0;
-    String[] arr = {"A", "E", "I", "O", "U"};
+    String[] str = {"A", "E", "I", "O", "U"};
     
     public int solution(String word) {
+        
+        dfs(word, "");
 
-        dfs("",word);        
         return answer;
     }
     
-    public void dfs(String current, String word) {
+    public void dfs(String word, String curr) {
         
-        if (current.equals(word)) {
+        if (word.equals(curr)) {
             answer = count;
-            // 단순히 함수를 끝내는 return
             return;
         }
         
-        // 5자리까지만 길이를 지정
-        if (current.length() == 5) {
+        if (curr.length() == 5) {
             return;
         }
         
-        for (int i = 0; i < 5; i++) {
-            // count는 만들어진 단어의 횟수
+        for(int i = 0; i < 5; i++) {
             count++;
-            dfs(current + arr[i], word);
+            dfs(word, curr + str[i]);
         }
+        
+        
     }
 }
