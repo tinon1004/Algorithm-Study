acd8fbf34afa8abbda81ce16886a23d772c25cd4
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-02-19 14:00:48 +0900

    [level 2] Title: 의상, Time: 0.16 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -10,12 +10,10 @@ class Solution {
             map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
         }
         
-        
-        for (int i : map.values()) {
-            answer *= (i + 1);
+        for (String key : map.keySet()) {
+            answer *= map.get(key) + 1;
         }
         
-        // 최소 1개라도 착용해야하기 때문에 아예 안 입는 경우 1 차감
         answer -= 1;
         
         return answer;
