8ba0f6816a78540e1a198a3e226e0e34907b8544
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-02-19 16:17:13 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -7,13 +7,11 @@ class Solution {
         int answer = 0;
         visited = new boolean[n];
         
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < visited.length; i++) {
             if(!visited[i]) {
                 dfs(i, computers);
                 answer++;
             }
-                            
-
         }
         
         return answer;
@@ -24,7 +22,7 @@ class Solution {
         visited[n] = true;
         
         for (int i = 0; i < computers.length; i++) {
-            if(!visited[i] && computers[n][i] == 1) {
+            if(computers[n][i] == 1 && !visited[i]) {
                 dfs(i, computers);
             }
         }
