e12b3ec19fa32940d5fb2fc429acfd37dc7f404a
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-02-19 10:13:52 +0900

    [level 0] Title: 외계어 사전, Time: 0.69 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -2,20 +2,19 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] spell, String[] dic) {
-        int answer = 0;
         
         Arrays.sort(spell);
-        String str = String.join("",spell);
-        
+        String s = String.join("", spell);
         
         for (int i = 0; i < dic.length; i++) {
-            String[] dic_str = dic[i].split("");
-            Arrays.sort(dic_str);
-            String str2 = String.join("",dic_str);
             
-            if (str2.equals(str)) {
+            String[] str = dic[i].split("");
+            Arrays.sort(str);
+            String s2 = String.join("",str);
+            
+            if (s2.equals(s)) {
                 return 1;
-            } 
+            }
         }
         
         return 2;
