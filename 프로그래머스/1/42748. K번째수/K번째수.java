983d4d6807ffa6bb2b1808bdb4747052dea2aa9f
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-08-01 06:41:37 +0900

    [level 1] Title: K번째수, Time: 1.50 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,19 +2,25 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
+        int[] answer = new int[commands.length];    
         
-        for (int a = 0; a < commands.length; a++) {
-            int i = commands[a][0];
-            int j = commands[a][1];
-            int k = commands[a][2];
+        for (int i = 0; i < commands.length; i++) {
             
-            int[] arr = Arrays.copyOfRange(array, i - 1, j);
+            int num1 = commands[i][0];
+            int num2 = commands[i][1];
+            int num3 = commands[i][2];
             
-            Arrays.sort(arr);
+            int[] temp = new int[num2 - num1 + 1];
+            int idx = 0;
             
-            answer[a] = arr[k - 1];
+            for (int j = num1 - 1; j <= num2 -1; j++) {
+                temp[idx] = array[j];
+                idx++;
+            }
             
+            Arrays.sort(temp);
+            
+            answer[i] = temp[num3 -1];
         }
         
         return answer;
