98e875a211ebce76ef4cfb3601e06a85de40bcc2
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-08-01 06:22:19 +0900

    [level 2] Title: 더 맵게, Time: 674.01 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -4,6 +4,7 @@ class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
         
+        //우선순위큐를 사용하는 이유는 -> 가장 작은 음식 1,2 찾아야 하기 때문
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         
         for (int i = 0; i < scoville.length; i++) {
@@ -12,17 +13,18 @@ class Solution {
         
         while (pq.peek() < K) {
             
-            // pq에서는 1개가 남았을 때 poll 해버리면 오류가 남
+            // 우선순위큐에서는 1개가 남았을 때, poll 하면 오류남
             if (pq.size() < 2) {
                 return -1;
             }
             
+            int ns = 0;
             int num1 = pq.poll();
             int num2 = pq.poll();
             
-            int num3 = num1 + (num2 * 2);
+            ns = num1 + num2 * 2;
             
-            pq.add(num3);
+            pq.add(ns);
             
             answer++;
         }
