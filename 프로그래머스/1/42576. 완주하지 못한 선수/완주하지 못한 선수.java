6145a790b43a4ddb994798fabeaf6f223b723572
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-08-01 05:42:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 220.45 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,22 +4,15 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         
-        HashMap<String, Integer> map = new HashMap<>();
-        
-        for (int i = 0; i < participant.length; i++) {
-            map.put(participant[i], map.getOrDefault(participant[i],0) + 1);
-        }
+        Arrays.sort(participant);
+        Arrays.sort(completion);
         
         for (int i = 0; i < completion.length; i++) {
-            map.put(completion[i], map.get(completion[i]) - 1);
-        }
-        
-        for (String key : map.keySet()) {
-            if (map.get(key) > 0) {
-                answer = key;
+            if (!completion[i].equals(participant[i])) {
+                return participant[i];
             }
         }
         
-        return answer;
+        return participant[participant.length - 1];
     }
 }
