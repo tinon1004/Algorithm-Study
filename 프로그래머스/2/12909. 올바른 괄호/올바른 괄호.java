d4e9c9c86577aa7cf32dd7eec0f4a4480b863993
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-08-01 10:34:46 +0900

    [level 2] Title: 올바른 괄호, Time: 2.61 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,27 +1,30 @@
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        int sum = 0;
         
-        for (int i = 0; i < s. length(); i++) {
+        int count = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            
             char c = s.charAt(i);
             
             if (c == '(') {
-                sum++;
+                count++;
             } else {
-                sum--;
+                count--;
             }
             
-            if (sum < 0) {
+            if (count < 0) {
                 return false;
             }
             
         }
         
-        if (sum > 0) {
+        if (count > 0) {
             return false;
         }
-
+        
         return true;
+        
+        
     }
 }
