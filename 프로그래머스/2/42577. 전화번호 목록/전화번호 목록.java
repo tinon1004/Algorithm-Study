577b1d2a12801f754aee3ec2559829a9eb7a46c1
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-08-01 13:04:12 +0900

    [level 2] Title: 전화번호 목록, Time: 212.39 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -7,11 +7,11 @@ class Solution {
         Arrays.sort(phone_book);
         
         for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i+1].startsWith(phone_book[i])) {
+            if (phone_book[i + 1].startsWith(phone_book[i])){
                 return false;
             }
         }
         
-        return true;
+        return answer;
     }
 }
