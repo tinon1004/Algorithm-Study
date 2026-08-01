82c1fe43619b7655ba7b45868c97f56d88be5f50
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-08-01 12:57:39 +0900

    [level 2] Title: 의상, Time: 0.07 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,16 +3,13 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        
         HashMap<String, Integer> map = new HashMap<>();
         
         for (int i = 0; i < clothes.length; i++) {
             map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
         }
         
-        //map.keySet() -> 모든 key를 가져옴 
-        for (String key : map.keySet()) {
-            // 각 key에서 안 입는 경우 포함
+        for(String key: map.keySet()) {
             answer *= map.get(key) + 1;
         }
         
