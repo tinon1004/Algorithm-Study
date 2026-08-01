c718b8edcf6a0f8bf656c0cc82901b479d78786e
CHO NA AE <128207155+tinon1004@users.noreply.github.com>
2026-08-01 09:55:05 +0900

    [level 2] Title: 타겟 넘버, Time: 3.99 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -3,23 +3,31 @@ class Solution {
     int answer = 0;
     
     public int solution(int[] numbers, int target) {
-        
+       
         dfs(numbers, target, 0, 0);
-        
+       
         return answer;
     }
     
     public void dfs(int[] numbers, int target, int idx, int sum) {
-        
+        // 모든 숫자를 사용한 경우
         if (idx == numbers.length) {
-            if (target == sum) {
+            
+            // sum이 타겟 넘버인 경우
+            if (sum == target) {
                 answer++;
             }
+            
             return;
+            
         }
-        
-        dfs(numbers, target, idx + 1, sum + numbers[idx]);
-        dfs(numbers, target, idx + 1, sum - numbers[idx]);
+    
+    // 현재 숫자를 더하는 경우
+    dfs(numbers, target, idx + 1, sum + numbers[idx]);
+    
+    // 현재 숫자를 빼는 경우
+    dfs(numbers, target, idx + 1, sum - numbers[idx]);
         
     }
+
 }
